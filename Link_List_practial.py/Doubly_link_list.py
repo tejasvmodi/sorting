089bb19cf2c2bfc8dac2d9acc6c65bf70717974e
tejasvmodi <moditejasv@gmail.com>
2023-12-04 12:38:53 +0530

    commit done

---
+++ Link_List_practial.py/Doubly_link_list.py
@@ -0,0 +1,240 @@
+class Node:
+    def __init__(self, data):
+        self.prev = None
+        self.next = None
+        self.data = data
+
+class DoublyLinkList:
+    def __init__(self):
+        self.head = None
+
+    def InsertNode(self, value):
+        newNode = Node(value)
+
+        if self.head == None:
+            self.head = newNode
+            newNode.prev=None
+            newNode.next=None
+        else:
+            temp = self.head
+            while temp.next != None:
+                temp = temp.next
+            newNode.prev = temp
+            temp.next = newNode
+
+    def forwardprint(self):
+        if self.head == None:
+            print("List is Empty")
+        else:
+            temp = self.head
+            while temp != None:
+                print(temp.data,end=" ")
+                temp = temp.next
+
+    def backwordPrint(self):
+        if self.head == None:
+            print("List is empty")
+        else:
+            temp = self.head
+            while temp.next != None:
+                temp = temp.next
+
+            while temp != None:
+                print(temp.data,end=" ")
+                temp = temp.prev
+
+    def count(self):
+        if self.head == None:
+            print("List is empty")
+        else:
+            count = 0
+            temp = self.head
+            while temp != None:
+                count = count + 1
+                temp = temp.next
+
+            print("count of the item in list is", count)
+    
+    def search(self,val):
+        if self.head == None:
+            print("List is empty ")
+        else:
+            temp=self.head
+            flag=0
+            while temp != None :
+                if temp.data == val :
+                    print ("value search")
+                    flag=1
+                temp=temp.next
+                
+            if flag==0 :
+                print("value not in list ")
+    def insertAfter(self,key,value):
+        newNode=Node(value)
+        if self.head == None:
+            self.head = newNode
+            newNode.prev=None
+            newNode.next=None
+        else:
+            temp=self.head
+            while temp != None :
+                if temp.data == key :
+                    newNode.next = temp.next
+                    newNode.prev=temp
+                    temp.next=newNode
+                
+                temp=temp.next
+        
+    def inserBefore(self,key,value):
+        newNode=Node(value)
+        if self.head == None:
+            self.head = newNode
+            newNode.prev=None
+            newNode.next=None
+        else:
+            if self.head.data==key :
+                newNode.prev = None
+                newNode.next=self.head
+                self.head.prev=newNode
+                self.head=newNode
+            else:
+                temp= self.head
+                while temp.next != None and temp.next.data!=key:
+                    temp= temp.next
+
+                if temp.next!=None :
+                    newNode.next = temp.next
+                    newNode.prev= temp
+                    temp.next.prev=newNode
+                    temp.next=newNode
+                    
+                    
+    def deletebyKey(self,key):
+        if self.head==None:
+            print ("List is empty ")
+        else:
+            if self.head.data==key :
+                temp=self.head
+                self.head=self.head.next
+                self.head.prev=None
+                del temp
+            else:
+                temp = self.head
+                while temp != None :
+                    if temp.data == key:
+                        temp.prev.next=temp.next
+                        temp.next.prev=temp.prev
+                        del temp
+                        break
+                    temp=temp.next
+                else:
+                    print("no value deleted in list ")
+                
+    
+    def deletebyposition(self, position):
+        if self.head == None:
+            print("List is empty")
+        else:
+            if position == 1:
+                temp = self.head
+                self.head = self.head.next
+                if self.head != None:
+                    self.head.prev = None
+                del temp
+            else:
+                temp = self.head
+                current_position = 1
+
+                while current_position < position and temp != None:
+                    temp = temp.next
+                    current_position += 1
+
+                if temp == None:
+                    print(f"Position {position} is out of bounds")
+                else:
+                    if temp.prev !=None:
+                        temp.prev.next = temp.next
+                    if temp.next != None:
+                        temp.next.prev = temp.prev
+                    del temp
+
+dll=DoublyLinkList()
+while True:
+    print("1 Insert the node")
+    print("2 Forward Print ")
+    print("3 Backword print ")
+    print("4 Count the item in list")
+    print("5 search the item in list ")
+    print("6 insert after the ")
+    print("7 insert before the ")
+    print("8 Delete by key ")
+    print("9 Delete by position ")
+    print("10 exit ")
+    choice=int(input("enter your choice"))
+    match choice:
+        case 1:
+            c=int(input("count of element"))
+            for i in range(0,c):
+                i=input("enter the value")
+                dll.InsertNode(i)
+            print()
+            print()
+            print()
+        case 2:
+            dll.forwardprint()
+            print()
+            print()
+            print()
+        case 3:
+            dll.backwordPrint()
+            print()
+            print()
+            print()
+        case 4:
+              dll.count()
+              print()
+              print()
+              print()
+        case 5:
+            i=input("enter the value")
+            dll.search(i)
+            print()
+            print()
+            print()
+        case 6: 
+             k=input("enter key")
+             i=input("enter the value")
+             dll.insertAfter(k,i)
+             print()
+             print()
+             print()
+        case 7:
+              k=input("enter key")
+              i=input("enter the value")
+              dll.inserBefore(k,i)
+              print()
+              print()
+              print()
+        case 8:
+             i=input("enter the value")
+             dll.deletebyKey(i)
+             print()
+             print()
+             print()
+        case 9:
+             i=input("enter the value")
+             dll.deletebyposition(i)
+             print()
+             print()
+             print()
+        case 10: 
+            exit()
+        case default:
+            print("you enter other keyword")
+
+
+
+                
+
+
+        
